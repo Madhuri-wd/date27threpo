db6cc3634fb009f5bd2d388e1951865517857355
madhurieee22@gmail.com <madhurieee22@gmail.com>
2025-11-27 22:48:14 +0530

    11thdone

---
+++ 11th.js
@@ -0,0 +1,12 @@
+console.log("Task 1");
+console.log("Task 2");
+console.log("Task 3");
+//////
+
+function printTasks() {
+  console.log("Task 1");
+  console.log("Task 2");
+  console.log("Task 3");
+}
+
+printTasks(); // Synchronous call
